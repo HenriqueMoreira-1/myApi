467f492471edd5dc0bfecf4bf7ccc6f881b48932
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-06 20:22:44 -0300

    refactor: separate routes into its own folder

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,13 +2,14 @@ import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { routes } from './routes'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' })
-})
+
+app.use(routes)
+
 app.listen(process.env.PORT, () =>
   console.log(`Server started on port ${process.env.PORT}!`),
 )
