be8bacb2399f72be593a8e802ba9655e4610ad8a
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-07 19:58:12 -0300

    refactor: move all my business rules into use case

--- src/roles/useCases/createRole/CreateRoleController.ts
+++ src/roles/useCases/createRole/CreateRoleController.ts
@@ -1,18 +1,13 @@
-import { RolesRepository } from '@roles/repositories/RolesRepository'
-import { AppError } from '@shared/error/AppError'
 import { Request, Response } from 'express'
+import { CreateRoleUseCase } from './CreateRoleUseCase'
 
 export class CreateRoleController {
+  constructor(private createRoleUseCase: CreateRoleUseCase) {}
+
   handle(request: Request, response: Response): Response {
     const { name } = request.body
-    const rolesRepository = new RolesRepository()
+    const role = this.createRoleUseCase.execute({ name })
 
-    const roleAlreadyExists = rolesRepository.findByName(name)
-    if (roleAlreadyExists) {
-      throw new AppError('Role already exists!')
-    }
-
-    const role = rolesRepository.create({ name })
     return response.status(201).json(role)
   }
 }
