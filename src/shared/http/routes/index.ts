6cd23e0275c962ac7d64918367f260caaee94e8b
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-06 21:23:10 -0300

    feat: add roles route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-
+import { rolesRouter } from '@roles/http/routes/roles.routes'
 const routes = Router()
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello World' })
 })
+
+routes.use('/roles', rolesRouter)
 
 export { routes }
