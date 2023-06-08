4817c59f16a4a6a45fe8ae5ead2e016c09778017
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-08 00:34:47 -0300

    feat: create migration CreateRolesTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1686194422328 } from './migrations/1686194422328-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1686194422328],
 })
