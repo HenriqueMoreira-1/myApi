cfa8aa1539e6d596419cb2c61ef1b52b68632963
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-09 11:54:06 -0300

    feat: create use case showRole

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,5 +1,6 @@
 import { createRoleController } from '@roles/useCases/createRole'
 import { listRolesController } from '@roles/useCases/listRoles'
+import { showRoleController } from '@roles/useCases/showRole'
 import { Router } from 'express'
 
 const rolesRouter = Router()
@@ -10,6 +11,10 @@ rolesRouter.post('/', (request, response) => {
 
 rolesRouter.get('/', (request, response) => {
   return listRolesController.handle(request, response)
+})
+
+rolesRouter.get('/:id', (request, response) => {
+  return showRoleController.handle(request, response)
 })
 
 export { rolesRouter }
---
+++ src/roles/useCases/showRole/index.ts
@@ -0,0 +1,9 @@
+import { RolesRepository } from '@roles/repositories/RolesRepository'
+import { ShowRoleUseCase } from './ShowRoleUseCase'
+import { ShowRoleController } from './ShowRoleController'
+
+const rolesRepository = RolesRepository.getInstance()
+const showRoleUseCase = new ShowRoleUseCase(rolesRepository)
+const showRoleController = new ShowRoleController(showRoleUseCase)
+
+export { showRoleController }
