a1f685a1c772c0a32cdba191d2c7112dc2bda534
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-09 14:10:14 -0300

    feat: create updateRole use case

---
+++ src/roles/useCases/updateRole/UpdateRoleUseCase.ts
@@ -0,0 +1,29 @@
+import { Role } from '@roles/entities/Role'
+import { RolesRepository } from '@roles/repositories/RolesRepository'
+import { AppError } from '@shared/error/AppError'
+
+type UpdateRoleDTO = {
+  id: string
+  name: string
+}
+
+export class UpdateRoleUseCase {
+  constructor(private rolesRepository: RolesRepository) {}
+
+  async execute({ id, name }: UpdateRoleDTO): Promise<Role> {
+    const role = await this.rolesRepository.findById(id)
+    const roleWithSameName = await this.rolesRepository.findByName(name)
+
+    if (!role) {
+      throw new AppError('Role not found!', 404)
+    }
+
+    if (roleWithSameName) {
+      throw new AppError('Role name not informed or already in use!')
+    }
+
+    role.name = name
+
+    return this.rolesRepository.save(role)
+  }
+}
---
+++ src/roles/useCases/updateRole/index.ts
@@ -0,0 +1,9 @@
+import { RolesRepository } from '@roles/repositories/RolesRepository'
+import { UpdateRoleUseCase } from './UpdateRoleUseCase'
+import { UpdateRoleController } from './UpdateRoleController'
+
+const rolesRepository = RolesRepository.getInstance()
+const updateRoleUseCase = new UpdateRoleUseCase(rolesRepository)
+const updateRoleController = new UpdateRoleController(updateRoleUseCase)
+
+export { updateRoleController }
