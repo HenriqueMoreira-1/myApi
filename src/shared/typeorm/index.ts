852ef6081026c878cd041c9cc9bf50540c789658
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-16 11:34:19 -0300

    feat: create RefreshToken entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -5,11 +5,12 @@ import { CreateUsersTable1686359793425 } from './migrations/1686359793425-Create
 import { AddRoleIdToUsersTable1686360415239 } from './migrations/1686360415239-AddRoleIdToUsersTable'
 import { User } from '@users/entities/User'
 import { CreateRefreshTokensTable1686925153136 } from './migrations/1686925153136-CreateRefreshTokensTable'
+import { RefreshToken } from '@users/entities/RefreshToken'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role, User],
+  entities: [Role, User, RefreshToken],
   migrations: [
     CreateRolesTable1686194422328,
     CreateUsersTable1686359793425,
